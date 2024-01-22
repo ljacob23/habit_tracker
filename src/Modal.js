e06a225002b9fa4cc1295836c397c0160d3d1e28
ljacob23 <ljacob23@live.unc.edu>
2024-01-21 22:35:07 -0500

    Added top calendar. Still need to match buttons to calendar buttons

--- src/Modal.js
+++ src/Modal.js
@@ -3,8 +3,8 @@ import './Modal.css';
 
 const Modal = ({ isOpen, onClose, date, habitData, setHabitData }) => {
     const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    // const [modalOpen, setModalOpen] = useState(false);
-    // const [selectedDate, setSelectedDate] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
     const checkBox = (habit) => {
         setHabitData((prevData) => {
             const dateISO = date.toISOString();
@@ -31,41 +31,47 @@ const Modal = ({ isOpen, onClose, date, habitData, setHabitData }) => {
     const dateHabitData = habitData[dateISO] || {}; 
 
 
-    // const getCurrentWeek = () => {
-    //     const currentDay = date.getDate();
-    //     const startOfWeek = new Date(date);
-    //     //sets to sunday
-    //     startOfWeek.setDate(currentDay - date.getDay());
-    //     const week = Array.from({length: 7 }, (_, index) => new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + index));
+    const getCurrentWeek = () => {
+        const currentDay = date.getDate();
+        const startOfWeek = new Date(date);
+        //sets to sunday
+        startOfWeek.setDate(currentDay - date.getDay());
+        const week = Array.from({length: 7 }, (_, index) => new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + index));
 
-    //     return week;
-    // }; 
+        return week;
+    }; 
 
-    // const handleDayClick = (clickedDate) => {
-    //     setSelectedDate(clickedDate);
-    //     setModalOpen(true);
-    // };
+    const handleDayClick = (clickedDate) => {
+        setSelectedDate(clickedDate);
+        setModalOpen(true);
+    };
 
     const closeModal = ()  => {
         onClose();
     };
 
 
-    // const week = getCurrentWeek();
+    const week = getCurrentWeek();
+    console.log(week);
 
     return (
         <div className="modal-overlay">
           <div className="modal">
             <button onClick={closeModal}>&laquo; Back</button>
-            {/* <div className = "week-header">
+            <h2>{new Date().toLocaleString('default', {month : 'short'})} {new Date().getFullYear()} </h2>
+            <div className = "week-header">
                 {week.map((day, index) => (
                     <div 
                     key={index} 
                     className = "day"
                     onClick={() => handleDayClick(day)}>
+                        <div className = "individual">
+                        <p className="dayOfWeek">{day.toLocaleDateString('en-US', {weekday: 'short' })}</p>
+                        <p className="date">{day.getDate()}</p>
+                        </div>
                     </div>
                 ))}
-            </div> */}
+            </div>
             <div className = "habits">
             <label className = "task">
               <input
